9ca314d21823c622be693e30ab72033d1a6fb68e
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-16 22:44:33 -0800

    Added fetch request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    images: []
+  }
+  searchSubmitHandler = (searchTerm) => {
+    let url = 'https://api.unsplash.com/search/photos?query='
+
+    fetch(`${url}${searchTerm}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Client-ID zujqwoWD1PfY8h1azpRRURgOlxHsoTjr5Zcju_uExe4"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Something went wrong!');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => this.setState({ images: data.results }))
+      .catch(err => console.log(err));
+  }
+  render() {
+    let images = this.state.results.map(result => <img src={`${result.urls.small}`} alt=''></img>)
+    return (
+      <div className="container">
+        <SearchBar submitForm={this.searchSubmitHandler} />
+        {images}
+      </div>
+    );
+  }
 }
 
 export default App;
